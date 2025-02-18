fe5bd6926d6a8087e557a8037c4f74a7cf8a5647
ptCady1 <ptCady0@gmail.com>
2025-02-18 15:36:17 -0600

    This is just a basic git piece for school.

---
+++ tutorial/src/main/java/com/mycompany/tutorial/Tutorial.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tutorial;
+
+/**
+ *
+ * @author Paul
+ */
+public class Tutorial {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
